f48099d04bb49de257ea43afff0d70bb20884527
joaquin <jgamboa007@gmail.com>
2020-08-12 16:44:36 -0400

    test1

--- src/components/Revisar/Home.js
+++ src/components/Revisar/Home.js
@@ -18,6 +18,7 @@ export function Home() {
             const ordenesFormateadas = formatearArregloColumnas(resOrdenes,4); 
             setOrdenes(ordenesFormateadas);
         }
+        const a=1;
     };
 
     useEffect(() => {
